42c5a4699c74dd87086bdc3dfbc21b0b30c4653c
Antonio MM <antoniomm80@gmail.com>
2023-07-12 18:13:08 +0100

    feat: Posicionamiento de mapa en la tarjeta de la vista de detalle feat: Prototipo de react hook form

--- src/runtracker/components/tracklistpage.tsx
+++ src/runtracker/components/tracklistpage.tsx
@@ -7,7 +7,7 @@ import { useMediaQuery } from "@mantine/hooks";
 export function TrackListPage() {
     const tracksSummary = useRunTrackerStore((state) => state.tracksSummary);
     const open = useRunTrackerStore((state) => state.open);
-    const isMobile = useMediaQuery('(max-width: 768px)');
+    const isMobile = useMediaQuery('(max-width: 48em)');
     return (
         <>
             <TrackList tracks={tracksSummary} />
--- src/runtracker/components/trackpage.tsx
+++ src/runtracker/components/trackpage.tsx
@@ -1,33 +1,75 @@
-import {Button, Container, Group, Modal, Space, TextInput} from "@mantine/core";
-import {TimeProps} from "../domain/time";
-import {TrackProps} from "../domain/track";
-import {TrackCard} from "./trackcard";
-import {useDisclosure} from "@mantine/hooks";
-import {IconClock} from "@tabler/icons-react";
-import {DateInput} from "@mantine/dates";
-import {translate} from "react-i18nify";
+import { Button, Container, Group, Modal, Space, TextInput } from "@mantine/core";
+import { TimeProps } from "../domain/time";
+import { Track, TrackProps } from "../domain/track";
+import { TrackCard } from "./trackcard";
+import { useDisclosure } from "@mantine/hooks";
+import { IconCalendar, IconClock } from "@tabler/icons-react";
+import { DateInput } from "@mantine/dates";
+import { translate } from "react-i18nify";
 import { TrackSummary } from "../domain/tracksummary";
+import { pathService } from "../domain/trackservice";
+import { useQuery } from "@tanstack/react-query";
+import { useParams } from "react-router-dom";
+
+import { useForm, Resolver } from "react-hook-form";
+
+type FormValues = {
+    trainingDate: string;
+    duration: string;
+};
+
+
 
 export interface TrackPageProps {
     trackSummary?: TrackSummary;
 }
 
 export function TrackPage(props: TrackPageProps) {
-    const [opened, {open, close}] = useDisclosure(false);
-    if(props.trackSummary === undefined) return (<></>);
+    const {
+        register,
+        formState: { errors },
+        handleSubmit,
+      } = useForm();
+    const onSubmit = handleSubmit((data) => alert(data.duration));
+    const [opened, { open, close }] = useDisclosure(false);
+    let { trackId } = useParams();
+    const { isLoading, data } = useQuery(["path", trackId], () => pathService.findById(trackId || ""));
+    if (isLoading) {
+        return <></>;
+    }
+    const track = new Track(data as TrackProps);
     return (
         <>
             <Container fluid>
-                <TrackCard bestTime={props.trackSummary.bestTime} track={props.trackSummary} open={open}/>
+                <TrackCard bestTime={props.trackSummary.bestTime} track={track} open={open} />
             </Container>
             <Modal opened={opened} onClose={close} size="md" title={translate("newTime.title")} centered>
-                <DateInput placeholder={translate("newTime.trainingDate")} label={translate("newTime.trainingDate")} withAsterisk/>
-                <Space h="sm"/>
-                <TextInput label={translate("newTime.durationString")} placeholder="mm:ss" icon={<IconClock size="0.8rem"/>} withAsterisk/>
-                <Space h="lg"/>
-                <Group position="right">
-                    <Button>{translate("actions.save")}</Button>
-                </Group>
+                <form onSubmit={onSubmit}>
+                <TextInput
+                        label={translate("newTime.durationString")}
+                        placeholder="mm:ss"
+                        icon={<IconCalendar size="0.8rem" />}
+                        type="date"
+                        {...register("trainingDate",{ required: true})}
+                        aria-invalid={errors.trainingDate ? "true" : "false"}
+                        withAsterisk
+                    />
+                    {errors.trainingDate && <span>{translate("validation.mandatory")}</span>}
+                    <Space h="sm" />
+                    <TextInput
+                        label={translate("newTime.durationString")}
+                        placeholder="mm:ss"
+                        icon={<IconClock size="0.8rem" />}
+                        {...register("duration",{ required: true })}
+                        aria-invalid={errors.duration ? "true" : "false"}
+                        withAsterisk
+                    />
+                    {errors.duration && <span>{translate("validation.mandatory")}</span>}
+                    <Space h="lg" />
+                    <Group position="right">
+                        <Button onClick={() => onSubmit()}>{translate("actions.save")}</Button>
+                    </Group>
+                </form>
             </Modal>
         </>
     );
