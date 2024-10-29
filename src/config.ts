43339808009b4e7b690bbce256a10cf78fa73790
AntonioMM <a.morcillo@weguest.com>
2024-10-29 19:04:45 +0100

    fix: cambia nombre host backend producción

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
 console.log(`Loading ${import.meta.env.PROD} config...`);
 
-export const SERVER_URL = import.meta.env.PROD ? 'http://black-pearl.local/run-tracker/api/v1' : 'http://localhost:3000';
+export const SERVER_URL = import.meta.env.PROD ? 'http://controlplane.local/run-tracker/api/v1' : 'http://localhost:3000';
 export const APP_PATH = import.meta.env.PROD ? '/tracks-app' : ''; 
