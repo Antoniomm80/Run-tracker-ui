7af6d8e13bdbf13ea8cd67cda71bd4d90e55a648
AntonioMM <a.morcillo@weguest.com>
2024-11-01 12:04:43 +0100

    fix: Corrige renderizado tiempos

--- src/runtracker/utils/timeutils.ts
+++ src/runtracker/utils/timeutils.ts
@@ -1,27 +1,27 @@
-import { TrackProps } from "../domain/track";
-import { Time, TimeProps } from "../domain/time";
+import {TrackProps} from "../domain/track";
+import {Time, TimeProps} from "../domain/time";
 
 
 const padLeft = (n: number): string => {
     return ("00" + n).slice(-2);
 }
 
-const renderTime = (minutes:number,seconds:number): string => {
-    return seconds > 10 ? `${minutes}:${seconds.toString()}` : `${minutes}:0${seconds.toString()}`;
+const renderTime = (minutes: number, seconds: number): string => {
+    return seconds >= 10 ? `${minutes}:${seconds.toString()}` : `${minutes}:0${seconds.toString()}`;
 }
 
 const timeUtils = {
-    printTime(time: number): string {        
+    printTime(time: number): string {
         const minutes = Math.floor(time / 60);
         const seconds = Math.floor(time % 60);
-        return renderTime(minutes,seconds);        
+        return renderTime(minutes, seconds);
     },
 
-    calculateSpeed(time:number,distance:number){
+    calculateSpeed(time: number, distance: number) {
         const secondsPerKilometer = (time / distance) * 1000;
         const minutes = Math.floor(secondsPerKilometer / 60);
         const seconds = Math.floor(secondsPerKilometer % 60);
-        return renderTime(minutes,seconds);
+        return renderTime(minutes, seconds);
     },
 
     formatDate(date: Date): string {
@@ -30,8 +30,8 @@ const timeUtils = {
             return `${dateChunks[2]}-${dateChunks[1]}-${dateChunks[0]}`;
         }
         const dformat: string = [padLeft(date.getDate()),
-        padLeft(date.getMonth() + 1),
-        date.getFullYear()
+            padLeft(date.getMonth() + 1),
+            date.getFullYear()
         ].join(' ');
         return dformat;
     },
